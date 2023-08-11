70111a00842d638c68c5eb9d25923e566f0ec37e
Yang Dong <yangdong0729@outlook.com>
2023-08-11 16:43:37 +0800

    初始化 ASC

---
+++ Source/Aura/Private/AbilitySystem/AuraAbilitySystemComponent.cpp
@@ -0,0 +1,5 @@
+// Copyright Yang Dong
+
+
+#include "AbilitySystem/AuraAbilitySystemComponent.h"
+
--- Source/Aura/Private/Player/AuraPlayerState.cpp
+++ Source/Aura/Private/Player/AuraPlayerState.cpp
@@ -3,7 +3,21 @@
 
 #include "Player/AuraPlayerState.h"
 
+#include "AbilitySystem/AuraAbilitySystemComponent.h"
+#include "AbilitySystem/AuraAttributeSet.h"
+
 AAuraPlayerState::AAuraPlayerState()
 {
 	NetUpdateFrequency = 100;
+
+	AbilitySystemComponent = CreateDefaultSubobject<UAuraAbilitySystemComponent>("AbilitySystemComponent");
+	AbilitySystemComponent->SetIsReplicated(true);
+	AbilitySystemComponent->SetReplicationMode(EGameplayEffectReplicationMode::Mixed);
+	
+	AttributeSet = CreateDefaultSubobject<UAuraAttributeSet>("AbilitySystemComponent");
+}
+
+UAbilitySystemComponent* AAuraPlayerState::GetAbilitySystemComponent() const
+{
+	return Cast<UAbilitySystemComponent>(AbilitySystemComponent);
 }
