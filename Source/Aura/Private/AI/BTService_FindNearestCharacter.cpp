687b50cbe935774af89f1d6792a1709f8ee74b26
Yang Dong <yangdong0729@outlook.com>
2023-10-27 10:50:34 +0800

    Find Nearest Character

---
+++ Source/Aura/Private/AI/BTService_FindNearestCharacter.cpp
@@ -0,0 +1,38 @@
+﻿// Copyright Yang Dong
+
+
+#include "AI/BTService_FindNearestCharacter.h"
+
+#include "AIController.h"
+#include "BehaviorTree/BTFunctionLibrary.h"
+#include "Kismet/GameplayStatics.h"
+
+void UBTService_FindNearestCharacter::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
+{
+	Super::TickNode(OwnerComp, NodeMemory, DeltaSeconds);
+
+	const APawn* OwningPawn = AIOwner->GetPawn();
+
+	const FName TargetTag = OwningPawn->ActorHasTag(FName("Player")) ? FName("Enemy") : FName("Player");
+
+	TArray<AActor*> ActorsWithTag;
+	UGameplayStatics::GetAllActorsWithTag(OwningPawn, TargetTag, ActorsWithTag);
+
+	float ClosestDistance = TNumericLimits<float>::Max();
+	AActor* ClosestActor = nullptr;
+	for (AActor* Actor : ActorsWithTag)
+	{
+		const float Distance = OwningPawn->GetDistanceTo(Actor);
+		if (Distance < ClosestDistance)
+		{
+			ClosestDistance = Distance;
+			ClosestActor = Actor;
+		}
+	}
+
+	if (IsValid(ClosestActor))
+	{
+		UBTFunctionLibrary::SetBlackboardValueAsObject(this, TargetToFollow, ClosestActor);
+		UBTFunctionLibrary::SetBlackboardValueAsFloat(this, DistanceToTarget, ClosestDistance);
+	}
+}
---
+++ Source/Aura/Public/AI/BTService_FindNearestCharacter.h
@@ -0,0 +1,25 @@
+﻿// Copyright Yang Dong
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/Services/BTService_BlueprintBase.h"
+#include "BTService_FindNearestCharacter.generated.h"
+
+UCLASS()
+class AURA_API UBTService_FindNearestCharacter : public UBTService_BlueprintBase
+{
+	GENERATED_BODY()
+
+protected:
+
+	virtual void TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds) override;
+
+private:
+
+	UPROPERTY(BlueprintReadOnly, EditAnywhere, meta = (AllowPrivateAccess))
+	FBlackboardKeySelector TargetToFollow;
+
+	UPROPERTY(BlueprintReadOnly, EditAnywhere, meta = (AllowPrivateAccess))
+	FBlackboardKeySelector DistanceToTarget;
+};
